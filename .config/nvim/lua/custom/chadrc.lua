2da606b3f8f366867d66aec9f716f1e6d760e9e2
Anthony Rave <anthony@prozon.com>
2024-09-13 21:25:42 +0200

    add chadrc.lua

---
+++ .config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,17 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+  theme = 'catppuccin',
+  theme_toggle = {
+    "catppuccin",
+    "catppuccin_light"
+  },
+  telescope = { style = "bordered" },
+  statusline = {
+    theme = "minimal",
+    separator_style = "round"
+  }
+}
+
+return M
